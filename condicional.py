459d19d199c1b6de751120afa36408320d1acfdb
fercasasnovas01 <43232730+fercasasnovas01@users.noreply.github.com>
2018-10-18 09:39:55 +0200

    Add files via upload

---
+++ condicional.py
@@ -0,0 +1,13 @@
+def condicional_2():
+    respuesta= raw_input("Que hora es (1-24) ")
+    if(respuesta>8):
+        print "buenos dias "+ nombre
+    else:
+        print "Adios "+ nombre
+
+
+condicional_1()
+
+                    
+
+       
